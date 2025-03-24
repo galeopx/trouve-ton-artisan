7af751be492dba5fd66de76be224cae437fe86ad
galeopx <galeopx@gmail.com>
2025-03-24 20:35:03 +0100

    correction erreur

--- api/server.js
+++ api/server.js
@@ -15,7 +15,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
